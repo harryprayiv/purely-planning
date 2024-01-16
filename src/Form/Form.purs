202cc17d76e5e23e24c5c2cb4531eef973896fd2
harryprayiv <harryprayiv@gmail.com>
2024-01-16 10:54:21 -0500

    modifying this code to work in my context.

--- src/Form/Form.purs
+++ src/Form/Form.purs
@@ -33,11 +33,8 @@ mkTime h m = Time <$> toEnum h <*> toEnum m <*> toEnum 0 <*> toEnum 0
 
 initialOptions ∷ Options
 initialOptions =
-  [ mkShift "F (06:45 - 15:30)" <$> (mkHours <$> mkTime 6 45 <*> mkTime 15 30)
-  , mkShift "C (12:30 - 21:30)" <$> (mkHours <$> mkTime 12 30 <*> mkTime 21 30)
-  , mkShift "C, weekend (13:30 - 21:30)" <$> (mkHours <$> mkTime 13 30 <*> mkTime 21 30)
-  , mkShift "M (08:30 - 17:30)" <$> (mkHours <$> mkTime 8 30 <*> mkTime 17 30)
-  , mkShift "N (20:45 - 07.15)" <$> (mkHours <$> mkTime 20 45 <*> mkTime 7 15)
+  [ mkShift "Available" <$> (mkHours <$> mkTime 0 0 <*> mkTime 23 59)
+  , mkShift "Booked" <$> (mkHours <$> mkTime 0 0 <*> mkTime 23 59)
   ]
 
 initialState ∷ Input → State
